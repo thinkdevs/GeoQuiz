602a8d6cc64059d0914b8ec324460ca0f33ee9d7
Clack Clack <clackclackcom@gmail.com>
2016-01-27 16:38:45 +0300

    Initial commit

---
+++ app/src/main/java/ru/mbraint/hardyphillips/geoquiz/TrueFalse.java
@@ -0,0 +1,28 @@
+package ru.mbraint.hardyphillips.geoquiz;
+
+public class TrueFalse {
+
+    private int mQuestion;
+    private boolean mTrueQuestion;
+
+    public TrueFalse(int mQuestion, boolean mTrueQuestion) {
+        this.mQuestion = mQuestion;
+        this.mTrueQuestion = mTrueQuestion;
+    }
+
+    public int getQuestion() {
+        return mQuestion;
+    }
+
+    public void setQuestion(int mQuestion) {
+        this.mQuestion = mQuestion;
+    }
+
+    public boolean isTrueQuestion() {
+        return mTrueQuestion;
+    }
+
+    public void setTrueQuestion(boolean mTrueQuestion) {
+        this.mTrueQuestion = mTrueQuestion;
+    }
+}
